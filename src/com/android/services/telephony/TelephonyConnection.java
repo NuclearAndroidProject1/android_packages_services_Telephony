4f5e74fc6e2f7340a5004842c978977c6195cfcb
Ethan Chen <intervigil@gmail.com>
2015-12-07 22:03:02 -0800

    Merge tag 'android-6.0.1_r3' into cm-13.0
    Android 6.0.1 release 3
    
    Change-Id: I5232ed6b3e4a691ff0f6e4a79c19dd91769e001a

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -897,6 +897,14 @@ abstract class TelephonyConnection extends Connection {
         }
         mIsMultiParty = mOriginalConnection.isMultiparty();
 
+        // updateState can set mOriginalConnection to null if its state is DISCONNECTED, so this
+        // should be executed *after* the above setters have run.
+        updateState();
+        if (mOriginalConnection == null) {
+            Log.w(this, "original Connection was nulled out as part of setOriginalConnection. " +
+                    originalConnection);
+        }
+
         fireOnOriginalConnectionConfigured();
     }
 
